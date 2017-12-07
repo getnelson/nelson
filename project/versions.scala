e42d960b84fb35b53ab7d31e52f1dcc4472c6a2c
Ross A. Baker <ross@rossabaker.com>
2017-12-07 15:05:01 -0500

    Bump http4s to 0.16.6.

--- project/versions.scala
+++ project/versions.scala
@@ -19,7 +19,7 @@ object V {
   val dispatch         = "0.11.2"
   val doobie           = "0.2.4"
   val http4sArgonaut61 = "0.16.0"
-  val http4s           = "0.16.3"
+  val http4s           = "0.16.6"
   val journal          = "3.0.18"
   val knobs            = "4.0.30-scalaz-7.1"
   val prometheus       = "0.0.21"
