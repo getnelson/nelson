1691f4fb6f1018539fc550e96c8e42131b1bf142
Timothy Perrett <tperrett@lyft.com>
2018-08-11 18:09:25 -0700

    guess we’re not propagating

--- .buildkite/pipeline.exec.sh
+++ .buildkite/pipeline.exec.sh
@@ -3,4 +3,10 @@
 # configure docker
 eval $(docker-machine env default)
 
+export TRAVIS="true" # way hacky
+export TRAVIS_COMMIT="$BUILDKITE_COMMIT"
+export TRAVIS_REPO_SLUG="getnelson/nelson"
+export TRAVIS_JOB_NUMBER="1.1"
+export TRAVIS_BUILD_NUMBER="$BUILDKITE_BUILD_NUMBER"
+
 sbt ++2.11.11 'release with-defaults'
