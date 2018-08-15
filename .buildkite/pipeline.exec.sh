01d2f9eeae487716901338494b6f215ab827b3e9
Timothy Perrett <tperrett@lyft.com>
2018-08-14 21:05:48 -0700

    Skips test while we validate signing

--- .buildkite/pipeline.exec.sh
+++ .buildkite/pipeline.exec.sh
@@ -13,4 +13,4 @@ export TRAVIS_REPO_SLUG="getnelson/nelson"
 export TRAVIS_JOB_NUMBER="1.1"
 export TRAVIS_BUILD_NUMBER="$BUILDKITE_BUILD_NUMBER"
 
-sbt ++2.11.11 'release with-defaults'
+sbt ++2.11.11 'release with-defaults skip-tests'
