eb216fb5802102031841198fa990b334804e116c
Timothy Perrett <tperrett@lyft.com>
2018-09-21 17:13:43 -0700

    Tidy the kube role creation

--- core/src/main/scala/Workflow.scala
+++ core/src/main/scala/Workflow.scala
@@ -144,9 +144,15 @@ object Workflow {
      *   policies=<stackname>
      */
     def writeKubernetesRoleToVault(dc: Datacenter, sn: StackName, ns: NamespaceName): WorkflowF[Unit] =
-      Vault.createKubernetesRole(dc.name, sn.toString,
-          List(sn.toString), List(ns.asString),
-          None, None, Some(List(policies.policyName(sn, ns)))).inject
+      Vault.createKubernetesRole(
+        authClusterName = dc.name,
+        roleName = sn.toString,
+        serviceAccountNames = List(sn.toString),
+        seviceAccountNamespaces = List(ns.asString),
+        defaultLeaseTTL = None,
+        maxLeaseTTL = None,
+        policies = Some(List(policies.policyName(sn, ns)))
+      ).inject
 
     def writeDiscoveryToConsul(id: ID, sn: StackName, ns: NamespaceName, dc: Datacenter): WorkflowF[Unit] =
       for {
