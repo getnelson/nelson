535701f92f62d5b375a6aa5173c8d2dd663e82e5
Timothy Perrett <tperrett@lyft.com>
2018-09-28 09:39:35 -0700

    Cleanup

--- .buildkite/pipeline.exec.sh
+++ .buildkite/pipeline.exec.sh
@@ -3,11 +3,6 @@
 git config --global user.email "team@getnelson.io"
 git config --global user.name "Nelson Team"
 
-# configure docker to use the remote tcp endpoint.
-# we're using 0.0.0.0 because 127.0.0.1 inside the
-# container still gets screwed up by docker networking.
-# export DOCKER_HOST="tcp://0.0.0.0:2375"
-
 # subvert the sbt-rig plugin
 export TRAVIS="true" # way hacky
 export TRAVIS_COMMIT="$BUILDKITE_COMMIT"
@@ -19,4 +14,4 @@ if [ "$BUILDKITE_PULL_REQUEST" = 'false' ]; then
 	git checkout -qf "$BUILDKITE_BRANCH";
 fi
 
-sbt ++2.11.11 test
+sbt ++2.11.11 'release with-defaults'
--- .buildkite/pipeline.teardown.sh
+++ .buildkite/pipeline.teardown.sh
@@ -4,4 +4,4 @@ echo "==>> initilizing doctl..."
 doctl auth init -t "${DIGITAL_OCEAN_API_TOKEN}"
 
 echo "==>> deleting the droplet..."
-doctl compute droplet delete -f buildkite-worker &
+doctl compute droplet delete -f buildkite-worker
