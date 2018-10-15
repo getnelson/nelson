c5802562c8ff946a778cd9109ba3da85886c1efc
Timothy Perrett <tperrett@lyft.com>
2018-10-14 17:25:23 -0700

    push it

--- .buildkite/extradition.exec.sh
+++ .buildkite/extradition.exec.sh
@@ -11,6 +11,8 @@ temp_dir=$(mktemp -d)
 git clone git@github.com:getnelson/api.git "${temp_dir}"
 cp -fR api/src/main/protobuf/  "${temp_dir}/src/main/protobuf/"
 
+cd "${temp_dir}"
+
 # save those changes
 git commit -am "auto-extradition $(date)"
 git push origin master
