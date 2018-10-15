22127a4838c5f74c342c399e8b9c9c70618f1235
Timothy Perrett <tperrett@lyft.com>
2018-10-14 17:29:47 -0700

    add some debug

--- .buildkite/extradition.exec.sh
+++ .buildkite/extradition.exec.sh
@@ -9,13 +9,17 @@ if [ "$BUILDKITE_PULL_REQUEST" = 'false' ]; then
 	git checkout -qf "$BUILDKITE_BRANCH";
 fi
 
+echo "--> cloning getnelson/api repo..."
 temp_dir=$(mktemp -d)
 git clone git@github.com:getnelson/api.git "${temp_dir}"
-cp -fR api/src/main/protobuf/  "${temp_dir}/src/main/protobuf/"
 
-cd "${temp_dir}"
+echo "--> copying updated files from nelson tree to api tree..."
+cp -fvR api/src/main/protobuf/  "${temp_dir}/src/main/protobuf/"
+
 
 # save those changes
+cd "${temp_dir}"
+echo "--> commiting changes to api upstream..."
 git commit -am "auto-extradition $(date)"
 git push origin master
 
