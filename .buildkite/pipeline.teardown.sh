972e46d01d6dc38a633db10f08b3799bb21f48a3
Nelson Team <team@getnelson.io>
2018-10-18 21:06:43 -0700

    Kill droplets with fire

--- .buildkite/pipeline.teardown.sh
+++ .buildkite/pipeline.teardown.sh
@@ -4,4 +4,4 @@ echo "==>> initilizing doctl..."
 doctl auth init -t "${DIGITAL_OCEAN_API_TOKEN}"
 
 echo "==>> deleting the droplet..."
-doctl compute droplet delete -f buildkite-worker
+doctl compute droplet list | grep -v 'ID' | sort -r -k1 | grep "buildkite-worker" | awk '{print $1}' | xargs -L1 doctl compute droplet delete -f
