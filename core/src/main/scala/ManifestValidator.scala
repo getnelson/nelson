340003a8ad050c8357af0fb6115bb62f3d02c64e
Timothy Perrett <458+timperrett@users.noreply.github.com>
2019-02-22 14:20:16 -0800

    Fix LB naming to match the validators expectations (#215)
    * Fix LB naming to match the validators expectations
    
    * update

--- core/src/main/scala/ManifestValidator.scala
+++ core/src/main/scala/ManifestValidator.scala
@@ -92,12 +92,13 @@ object ManifestValidator {
     units: List[UnitDef],
     whiteList: Option[ProxyPortWhitelist])(stg: StoreOp ~> IO): Valid[Unit] = {
 
-    // aws allows elb names to be at most 32 charasters. Nelson generates a name for
+    // NOTE: aws allows elb names to be at most 32 charasters. Nelson generates a name for
     // lbs that include 8 characters for a hash, 4 characters dashes and at least 1
     // character for version, for a total of 13. Giving the version some wiggle room for
     // a total of 15, so the max length of the name is 32 - 15
+    // For example: mypro--1-0-0--ssiididq
     def validateNameLength(lb: Manifest.Loadbalancer): Valid[Unit] =
-      if (lb.name.length <= 17) ().validNel
+      if (lb.name.length <= 12) ().validNel
       else InvalidLoadbalancerNameLength(lb.name).invalidNel
 
     def validateAllowedPorts(ps: Vector[Manifest.Port]): Valid[Unit] =
